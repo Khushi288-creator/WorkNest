51be3b220c2b718c426c1815b550d431ab5b4ace
Khushi <ktm407301@gmail.com>
2026-08-09 01:17:10 +0530

    feat: setup express server with middleware and MongoDB connection

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const app = express();
+dotenv.config();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/',(req,res) => {
+    res.send("Hello world");
+});
+
+mongoose.connect(process.env.MONGO_URI)
+   .then(() => { console.log("MongoDB connected successfully")})
+   .catch((error) => { console.log("MongoDB connection failed:", error)})
+
+app.listen(port,()=> {
+    console.log(`Example app listening on port ${port}`);
+});
